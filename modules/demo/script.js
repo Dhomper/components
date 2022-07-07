d3fba9ccbc94f9fa32a17d092a44d2ce75761733
Dhomper <dhomper@live.com.mx>
2022-07-07 17:01:26 -0500

    api ejemplo

--- modules/demo/script.js
+++ modules/demo/script.js
@@ -1,8 +1,31 @@
 angular.module('app').controller('components.demo', function($scope, $rootScope, $http)
 {
+	/// VAR
+
+	$scope.puestos = []
 	$scope.saludo = ""
+
+	/// FN
+
 	$scope.init = function()
 	{
 		$scope.saludo = prompt('Nombre')
+		$scope.listarPuestos()
+	}
+
+	/// API
+
+	$scope.listarPuestos = function()
+	{
+		$http.get(DWS.getURL("empleos/v1/puestos"),
+		{
+			params:
+			{
+			}
+
+		}).then(function(api)
+		{
+			$scope.puestos = api.data.response
+		})
 	}
 })
